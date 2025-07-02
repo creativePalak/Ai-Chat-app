11c6f7f641a3a7428e298b09896e3efb949a6016
Atul kumar <atul993418@gmail.com>
2025-07-03 02:09:15 +0530

    added env file just for api security

--- frontend/hello/src/socket/socket.js
+++ frontend/hello/src/socket/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
- const socket = io('http://localhost:3000' , {
+const Api = import.meta.env.VITE_SOCKET_API
+
+const socket = io( Api , {
     autoConnect : false
  });
 
